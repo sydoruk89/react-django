c79cc04de7886af95da7a452ded5067a4b0f182b
Roman Sydoruk <sydoruk89@gmail.com>
2020-10-27 20:36:09 -0700

    fixed refresh

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -3,42 +3,69 @@ import {
     SIGNUP_FAIL,
     LOGIN_SUCCESS,
     LOGIN_FAIL,
-    LOGOUT
+    LOGOUT,
+    AUTHENTICATED_FAIL,
+    AUTHENTICATED_SUCCESS,
+    USER_LOADED_SUCCESS,
+    USER_LOADED_FAIL
 } from '../actions/types';
 
 const initialState = {
-    token: localStorage.getItem('token'),
+    access: localStorage.getItem('access'),
+    refresh: localStorage.getItem('refresh'),
     isAuthenticated: null,
-    loading: false
+    user: null
 };
 
 export default function(state = initialState, action) {
     const { type, payload } = action;
 
     switch(type) {
+        case AUTHENTICATED_SUCCESS:
+            return {
+                ...state,
+                isAuthenticated: true
+            }
         case LOGIN_SUCCESS:
             localStorage.setItem('token', payload.access);
             return {
-                ...state,
                 isAuthenticated: true,
-                loading: false,
-                token: payload.access
+                access: payload.access,
+                refresh: payload.refresh
+            }
+        case USER_LOADED_SUCCESS:
+            return {
+                ...state,
+                user: payload
             }
         case SIGNUP_SUCCESS:
             return {
                 ...state,
                 isAuthenticated: false,
-                loading: true
+            }
+        case AUTHENTICATED_FAIL:
+            return {
+                ...state,
+                isAuthenticated: false
+            }
+        case USER_LOADED_FAIL:
+            return {
+                ...state,
+                user: null
             }
         case SIGNUP_FAIL:
         case LOGIN_FAIL:
         case LOGOUT:
-            localStorage.removeItem('token');
+            localStorage.removeItem('access');
+            localStorage.removeItem('refresh');
             return {
                 ...state,
+                access: null,
                 token: null,
+                refresh: null,
                 isAuthenticated: false,
-                loading: false
+                loading: false,
+                user: null
             }
         default:
             return state
